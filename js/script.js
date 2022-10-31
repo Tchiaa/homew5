3eca6d83175a6c12a112590b04090c5667b16822
Tchiaa <112972156+Tchiaa@users.noreply.github.com>
2022-11-01 00:38:56 +0400

    ff

--- js/script.js
+++ js/script.js
@@ -1,16 +1,49 @@
-var str=prompt ("შეიყვანეთ ასაკი");
-var income=1
-if (income >30)  {
-
-var message= "კარგი ასაკი"
+var str = prompt("შეიყვანეთ ასაკი");
+var income=50
+if (income >31)  {
+var message= ("კარგი ასაკი")
 
 alert (message)
 }
-else if ( income <=30 && >=51)
+else if ( income >=49 && income <=61)
 {
-alert(საშუალო ასაკი)
-}
+    var message=("საშუალო ასაკი")
+
+alert(message)
+}15
 
 else {
-    "დაისვენე ასაკი"
+    var message= ("დაისვენე ასაკი")
+    alert (message)
 }
+
+
+ var numbers1=[1,4,18,24,15,2,3,19,55];
+
+
+
+ console.log(numbers1 [1] [2] [3] [5])
+
+ console.log(numbers [8])
+
+ console.log(numbers [0])
+
+ var arr=[1,4,18,24,15,2,3,19,55];
+ for ( var i)=0; <arr.length; i++)
+ if (arr[i] % 2 === 0) {
+    console.log(arr[i])
+ }
+
+
+
+ 
+ // დავალება 2
+
+ var citys=["Paris", "london", "lisbon"];
+
+ for (var i = 0; i < citys.length; i ++)
+  {
+    console.log(citys[i])
+    document.write(citys[i]  + "</br>")
+    
+ }

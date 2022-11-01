d2b40a295093dbf8c4ad13fb84a716e74162dae7
Tchiaa <112972156+Tchiaa@users.noreply.github.com>
2022-11-01 17:05:30 +0400

    aa

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 var str = prompt("შეიყვანეთ ასაკი");
 var income=50
 if (income >31)  {
@@ -18,9 +19,7 @@ else {
 }
 
 
- var numbers1=[1,4,18,24,15,2,3,19,55];
-
-
+ var numbers1=[1,4,18,24,15,2,3,19,55]; // ეს გავაკეთე ხელით სანამ მეორე ლექციას ვუყურებდი
 
  console.log(numbers1 [1] [2] [3] [5])
 
@@ -29,8 +28,14 @@ else {
  console.log(numbers [0])
 
  var arr=[1,4,18,24,15,2,3,19,55];
- for ( var i)=0; <arr.length; i++)
- if (arr[i] % 2 === 0) {
+ for ( var int i)=0; <100; i++)
+ {int=x 
+    if (i%2 === 0)  
+
+
+  {x=0}
+
+ else { x=1}
     console.log(arr[i])
  }
 
@@ -47,3 +52,5 @@ else {
     document.write(citys[i]  + "</br>")
     
  }
+
+ 

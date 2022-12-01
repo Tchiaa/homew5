a6506bd4e0b34ded1ed797bc0d449b5f6f7a3757
Tchiaa <112972156+Tchiaa@users.noreply.github.com>
2022-12-01 13:28:42 +0400

    g

--- js/script.js
+++ js/script.js
@@ -11,33 +11,12 @@ else if ( income >=49 && income <=61)
     var message=("საშუალო ასაკი")
 
 alert(message)
-}15
+}
 
 else {
     var message= ("დაისვენე ასაკი")
     alert (message)
 }
-
-
- var numbers1=[1,4,18,24,15,2,3,19,55]; // ეს გავაკეთე ხელით სანამ მეორე ლექციას ვუყურებდი
-
- console.log(numbers1 [1] [2] [3] [5])
-
- console.log(numbers [8])
-
- console.log(numbers [0])
-
- var arr=[1,4,18,24,15,2,3,19,55];
- for ( var int i)=0; <100; i++)
- {int=x 
-    if (i%2 === 0)  
-
-
-  {x=0}
-
- else { x=1}
-    console.log(arr[i])
- }
 
 
 
@@ -53,17 +32,28 @@ else {
     
  }
 
- //oop
 
- var cinema= {
+  var numbers1 = [1, 4, 18, 24, 15, 2, 3, 19, 55]; // ეს გავაკეთე ხელით სანამ მეორე ლექციას ვუყურებდი
+
+  console.log(numbers1[1][2][3][5]);
+
+  console.log(numbers[8]);
+
+  console.log(numbers[0]);
+
+
+ // oop
+
+  var cinema= {
   name : "cavea",
   languages : [ "Georgian", "English", "french"],
   new film: {
+
     name: "Spiderman",
     IMD : 9
-  },
+  }
 
-  films : [
+  [
     {name: "film1", imd: 8},
     {name: "film2", imd: 9},
     {name: "fils3", imd: 10},
@@ -71,12 +61,7 @@ else {
   }
 
   document.write( "<h3> cavea მიმდინარე ფილმები </h3>")
-  for (var  i = 0; i < cinema.films.length; i++) {
-
+  for (var  i = 0; i < cinema.films.length; i++)
+   {
    document.write(cinema.films[i].name + "</br>")
   }
-
-
-
-
- 
